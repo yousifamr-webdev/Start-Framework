27c0347a54841709babc91eae4825e2fe9b23b7f
yousifamr-webdev <yousifamr811@gmail.com>
2025-07-21 06:10:45 +0300

    Update About.jsx
    Added text-center class to h2

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,7 +6,7 @@ export default function About() {
     <>
       <main className={Style["main-content"]}>
         <div className="container d-flex flex-column justify-content-center align-items-center">
-          <h2>ABOUT COMPONENT</h2>
+          <h2 className="text-center" >ABOUT COMPONENT</h2>
           <div className="d-flex justify-content-center align-items-center mb-3 mt-2">
             <span className={Style["line"]}></span>
             <i className="fa-solid fa-star text-white mx-4"></i>
